6702f0c370d49e110d1b67e3d637422ec71a7933
tenore44 <tenore44@tenorcup.com>
2021-08-31 09:59:58 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Book;
+use Illuminate\Http\Request;
+
+// 本ダッシュボード表示
+Route::get('/','BooksController@index');
+
+//登録処理
+Route::post('/books','BooksController@store');
+
+//更新画面
+Route::post('/booksedit/{books}', 'BooksController@edit');
+
+// 更新処理
+Route::post('/books/update', 'BooksController@update');
+
+// 本を削除
+Route::delete('/book/{book}', 'BooksController@destroy');
+
+// Auth
+Auth::routes();
+Route::get('/home', 'BooksController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    //welcomeページを表示
+    Route::get('/', function(){
+        return view('welcome');
+    });
+
+});
